a2b82a77ca55c1765cf1ad38af4e6fa86180dad2
cyio <ibeceo@gmail.com>
2018-07-08 14:19:49 +0000

    auto deploy

---
+++ precache-manifest.8395d0634ccfbc4769b34a2e588e1ee7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/fonts/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "126c3c918f5f2c2e44cf4e1541957579",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "4b35e12cf281ce42f4bd89ea355ff9db",
+    "url": "/handle/index.html"
+  },
+  {
+    "revision": "8d1623bc79e0608388d1",
+    "url": "/js/handle.8d1623bc.js"
+  },
+  {
+    "revision": "6b19eba54820856810f026c74c40abeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a011e48dda3615e138",
+    "url": "/js/index.79a011e4.js"
+  },
+  {
+    "revision": "7a43379a3f3263c08f44d37b0317c193",
+    "url": "/img/logo-bitcoincash-dark.part.7a43379a.svg"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/img/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/fonts/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "157290733a8cf64bd54e",
+    "url": "/js/chunk-vendors.15729073.js"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/fonts/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "b6f1679049955928b19a2e0fc4f936ad",
+    "url": "/fonts/fontello.b6f16790.eot"
+  },
+  {
+    "revision": "09448c5d5407366aa74ca6db959f31e9",
+    "url": "/fonts/fontello.09448c5d.ttf"
+  },
+  {
+    "revision": "2e9dbf4de3893afb4572b7f77079dc66",
+    "url": "/img/fontello.2e9dbf4d.svg"
+  },
+  {
+    "revision": "fc0f6ee2d0c6d06fe9fd8d799476c3a4",
+    "url": "/fonts/fontello.fc0f6ee2.woff"
+  },
+  {
+    "revision": "79a011e48dda3615e138",
+    "url": "/css/index.4aef9823.css"
+  },
+  {
+    "revision": "8d1623bc79e0608388d1",
+    "url": "/css/handle.0942321e.css"
+  },
+  {
+    "revision": "157290733a8cf64bd54e",
+    "url": "/css/chunk-vendors.af8ae7d7.css"
+  }
+];
